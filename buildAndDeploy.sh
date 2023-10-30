faa341923c9206af0e519a7de2930e166e1e58a0
Wellington Pires <49841026+wellingtonpires@users.noreply.github.com>
2023-10-30 12:01:30 -0300

    Update buildAndDeploy.sh

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -2,8 +2,8 @@
 
 #Script para encerrar o compose por completo, criar nova build e realizar o start, assim criando uma implantação limpa
 
-#sudo docker rm -vf $(docker ps -aq)
-#docker rmi -f $(docker images -aq)
+#sudo docker rm -vf $(sudo docker ps -aq)
+#sudo docker rmi -f $(sudo docker images -aq)
 sudo docker compose down -v
 sudo docker compose build
 sudo docker compose up
